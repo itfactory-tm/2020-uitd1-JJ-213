4661f76cc4f4186b4bdcc55a1d89eec9413ef8ed
TobiasGeys <r0737258@student.thomasmore.be>
2020-04-01 13:30:47 +0200

    favicon toegevoegd

--- dist/contact.php
+++ dist/contact.php
@@ -22,6 +22,17 @@
     <!-- CSS: eigen stylesheet -->
     <link rel="stylesheet" href="css/webBureau.css">
 
+
+    <!-- Favicon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+
+
 </head>
 <body>
 
